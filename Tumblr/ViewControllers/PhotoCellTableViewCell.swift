2230ef98e7922505821e1b2f1ea76da78c49dd23
Dieula <dareusdieula@gmail.com>
2018-09-24 12:42:27 -0700

    files

---
+++ Tumblr/ViewControllers/PhotoCellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCellTableViewCell.swift
+//  Tumblr
+//
+//  Created by Mac on 6/24/1397 AP.
+//  Copyright © 1397 Dieula. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
